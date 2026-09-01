0ad3662a8128a50563f9ab8ebbae970ec2ab072e
Jose Chiru <chirujavier12@gmail.com>
2026-09-01 11:19:57 -0500

    Agregando la clase Suma

---
+++ src/Suma.java
@@ -0,0 +1,29 @@
+public class Suma {
+    private int numA;
+    private int numB;
+    private int numC;
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int numA) {
+        this.numA = numA;
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int numB) {
+        this.numB = numB;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void setNumC(int numC) {
+        this.numC = numC;
+    }
+}
